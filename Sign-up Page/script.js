1d29c286cea3cc090a9f910fcc5ff0fd04137d14
PRIYANSHU SINGH <92785258+Priyanshusingh25@users.noreply.github.com>
2024-07-31 17:04:09 +0530

    Add files via upload
    Different UI than the the MARIO CLUB UI

---
+++ Sign-up Page/script.js
@@ -0,0 +1,20 @@
+alert("account created")
+
+// const btn = "document";
+
+// btn.getElementById(onclick ="one"), ()=> {
+//     console.log("www.google.com");
+// }
+
+// btn.getElementById(onclick = "two"), ()=> {
+//     console.log("www.github.com");
+// }
+// btn.getElementById(alert="three")  ,()=> {
+//     alert("Account Created")
+// }
+
+// onclick => changeColor();
+// function changeColor (){
+// document.getElementById("three");
+//     document.style.color="red";
+// }
